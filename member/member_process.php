39be68ac826ec1b74bc27d324211d7ae90d7342d
JKW1008 <todendvv91@gmail.com>
2023-07-20 22:22:21 +0900

    test

--- member/member_process.php
+++ member/member_process.php
@@ -1,10 +1,9 @@
 <?php
-    require_once('../DB/db.php');
+require_once('../db/db.php');
 
-    echo "<hr>";
-
-    switch($_GET['mode']){
-        case 'register' :
+if (isset($_GET['mode'])) {
+    switch ($_GET['mode']) {
+        case 'register':
             $id = $_POST['id'];
             $userid = $_POST['userid'];
             $pw1 = $_POST['pw1'];
@@ -14,14 +13,37 @@
             $tel = $_POST['tel'];
             $email = $_POST['email'];
 
-            // echo $id. '<br>'.$userid.'<br>'.$pw1.'<br>'.$pw2.'<br>'.$name.'<br>'.$sex.'<br>'.$tel.'<br>'.$email;
+            // 여기부터 수정된 부분
+            $sql = $conn->prepare('INSERT INTO register
+            (id, userid, pw, name, sex, tel, email, redate)
+            VALUES(:id, :userid, :pw, :name, :sex, :tel, :email, now())');
 
-            $sql = $conn -> prepare('INSERT INTO register(id, userid, pw, name, sex, tel, email, redate)
-                                    -- 이 부분은 DB 테이블에서 만든 이름과 똑같이 입력해줍니다.
-                                    VALUE(:id, :userid, :pw, :name, :sex, :tel, :email, now())');
+            $sql->bindParam(':id', $id);
+            $sql->bindParam(':userid', $userid);
+            $sql->bindParam(':pw', $pw1);
+            $sql->bindParam(':name', $name);
+            $sql->bindParam(':sex', $sex);
+            $sql->bindParam(':tel', $tel);
+            $sql->bindParam(':email', $email);
 
-            $sql -> bindParam(':id', $id);
-            
-        break;
+            $sql->execute();
+            // 여기까지 수정된 부분
+
+            header('location: ../main.php');
+            break;
+
+        // 다른 case들을 추가할 수 있습니다.
+        // 예: case 'update':
+        //       // 업데이트 처리 로직
+        //       break;
+
+        default:
+            // 잘못된 mode 값이 전달된 경우에 대한 처리
+            // 예: header('location: ../error.php');
+            break;
     }
+} else {
+    // 'mode' 값이 전달되지 않은 경우에 대한 처리
+    // 예: header('location: ../error.php');
+}
 ?>
