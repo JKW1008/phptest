a46adc36b24b09cb6a85f3a62a8dfcac27816f1b
JKW1008 <todendvv91@gmail.com>
2023-07-20 20:58:43 +0900

    test

--- member/member_process.php
+++ member/member_process.php
@@ -1,3 +1,16 @@
 <?php
     require_once('../DB/db.php');
+
+    switch($_GET['mode']){
+        case 'register' :
+            $id = $_POST['id'];
+            $userid = $_POST['userid'];
+            $pw1 = $_POST['pw1'];
+            $pw2 = $_POST['pw2'];
+            $name = $_POST['name'];
+            $sex = $_POST['sex'];
+            $tel = $_POST['tel'];
+            $email = $_POST['email'];
+        break;
+    }
 ?>
