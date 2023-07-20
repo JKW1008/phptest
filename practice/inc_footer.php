6f989eeb9ba5c53d61e132e0cc371e719cf5aa82
JKW1008 <todendvv91@gmail.com>
2023-07-20 23:54:18 +0900

    test

---
+++ practice/inc_footer.php
@@ -0,0 +1,13 @@
+<footer class="footer mt-5 py-3 bg-primary" width=100%>
+    <div class="container d-flex gap-1">
+        <img src="./images/logo.svg" alt="" style="width: 5rem" class="me-2" />
+        <div class="d-flex flex-column">
+            <span class="text-bg-primary">서울시 강남구 역산대로 123번지 왕초보빌딩 14층 (우)111134</span>
+            <span class="text-bg-primary">대표이사 : 정강우</span>
+            <span class="text-bg-primary">Te : 02-2222-44444 FAX : 02-2222-44444</span>
+        </div>
+    </div>
+</footer>
+</body>
+
+</html>
---
+++ practice/inc_header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+    <script src="./js/member.js"></script>
+    <title>약관</title>
+</head>
+
+<body>
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <a href="/"
+                class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                <img src="./images/logo.svg" alt="" style="width: 2rem" class="me-2" />
+                <span class="fs-4">test</span>
+            </a>
+
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a href="#" class="nav-link active" aria-current="page">Home</a>
+                </li>
+                <li class="nav-item"><a href="#" class="nav-link">회원가입</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">회사소개</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">게시판</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">로그인</a></li>
+            </ul>
+        </header>
