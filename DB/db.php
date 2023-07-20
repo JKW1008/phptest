03b801832061c3b241df73eea9c668a55fa0509a
JKW1008 <todendvv91@gmail.com>
2023-07-20 20:51:59 +0900

    test

---
+++ DB/db.php
@@ -0,0 +1,21 @@
+<?php
+try {
+    $host = "svc.sel4.cloudtype.app";
+    $db = "dsl";
+    $user = "test";
+    $pass = "1234";
+    $port = "30105";
+
+    // 연결 에러 시 예외를 던지도록 설정
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+
+    // PDO를 사용하여 접속 시도
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass, $options);
+    // $conn = mysqli_connect($host, $user, $pass, $db, $port);
+    echo "접속 성공!";
+} catch (PDOException $e) {
+    die("접속 실패: " . $e->getMessage());
+}
+?>
--- lib/connect_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $host = "svc.sel4.cloudtype.app";       
-    $db = "dsl"; 
-    $user = "test";
-    $pass = "1234";
-    $port = "30105";
-    
-    $conn = mysqli_connect($host, $user, $pass, $db, $port) or die("mysql서버 접속 에러");
-?>
