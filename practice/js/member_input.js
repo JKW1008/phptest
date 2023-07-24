a4f0824c4dd2481cbc63b6728d4072e97901a356
JKW1008 <todendvv91@gmail.com>
2023-07-25 00:39:56 +0900

    test

--- practice/js/member_input.js
+++ practice/js/member_input.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btn_id_check = document.querySelector("#btn_id_check");
   btn_id_check.addEventListener("click", () => {
-    alert(1);
+    const f_id = document.querySelector("#f_id");
+
+    if (f_id.value == "") {
+      alert("아이디를 입력해 주세요");
+      return false;
+    }
+
+    // AJAX
+    const f1 = new FormData();
+    f1.append("id", f_id.value);
+    f1.append("mode", "id_chk");
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "./pg/member.php", "true");
+    xhr.send();
+
+    xhr.onload = () => {
+      //
+    };
   });
 });
