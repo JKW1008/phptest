fdbd348a87c8ecd19ad0e68d1f4e1e9bc6cc4570
JKW1008 <todendvv91@gmail.com>
2023-08-04 22:20:28 +0900

    test

--- practice/admin/js/board.js
+++ practice/admin/js/board.js
@@ -10,6 +10,8 @@ document.addEventListener("DOMContentLoaded", () => {
       return false;
     }
 
+    btn_board_create.disabled = true;
+
     const xhr = new XMLHttpRequest();
 
     const f = new FormData();
